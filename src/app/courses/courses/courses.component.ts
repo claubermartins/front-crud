507830e56766cd4b0c783708e6201420a7e60f9f
Clauber Martins Pinto <clauber.pinto@mapa.magov.intranet>
2023-01-04 16:30:21 -0300

    11 - Lista de Cursos: Tratamento de Erros e MatDialog

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { catchError, Observable, of } from 'rxjs';
 
+import { ErrorDialogComponent } from './../../shared/components/error-dialog/error-dialog.component';
 import { Course } from './../model/course';
 import { CoursesService } from './../services/courses.service';
 
@@ -16,10 +18,24 @@ export class CoursesComponent implements OnInit {
 
   //coursesService: CoursesService;
 
-  constructor(private coursesService: CoursesService) {
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog
+  ) {
     //this.courses = [];
     //this.coursesService = new CoursesService();
-    this.courses$ = this.coursesService.list();
+    this.courses$ = this.coursesService.list()
+      .pipe(catchError(error => {
+        this.onError('Error ao carregar os cursos.');
+        return of([])
+      })
+      );
+  }
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
   }
 
   ngOnInit(): void {
