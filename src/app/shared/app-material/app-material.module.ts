902a72f9239893be6f2e05e0c492313201f4f242
“claubermartins” <clauber789@gmail.com>
2023-03-20 14:26:22 -0300

    38: Upgrade Angular v15 com Angular Material

--- src/app/shared/app-material/app-material.module.ts
+++ src/app/shared/app-material/app-material.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from '@angular/material/select';
